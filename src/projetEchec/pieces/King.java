e58269f574ffcc43154c9a13a923ad4d2dc1922e
Unicorn29A <youpi.killeur@gmail.com>
2017-05-11 18:27:40 +0200

    Creating child pieces classes

---
+++ src/projetEchec/pieces/King.java
@@ -0,0 +1,16 @@
+package projetEchec.pieces;
+
+import java.util.ArrayList;
+
+import projetEchec.UI.Board;
+import projetEchec.UI.Cell;
+
+public class King extends Piece {
+
+	@Override
+	public ArrayList<Cell> getPossibleDestinations(Board board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
